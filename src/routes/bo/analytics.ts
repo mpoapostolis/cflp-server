af6027ebe4d2e9b62c41c61133481cc6a6dea651
mpoapostolis <mpoapostolis@gmail.com>
2020-02-16 03:11:56 +0200

    fucked up commit

--- src/routes/bo/analytics.ts
+++ src/routes/bo/analytics.ts
@@ -8,6 +8,45 @@ import { subWeeks } from 'date-fns'
 
 const analytics = Router()
 
+analytics.get('/timeseries/:type', validateAdminToken, async (req: Request, res: Response) => {
+  const user = req.user as EmployeeToken
+  const { from = subWeeks(Date.now(), 1).getTime(), to = addWeeks(1)(Date.now()).getTime() } = req.query
+  const { type } = req.params
+
+  await MongoHelper.connect()
+  const data = await MongoHelper.db
+    .collection('transactions')
+    .aggregate([
+      {
+        $match: {
+          storeId: new ObjectId(user.storeId),
+          dateCreated: { $gte: new Date(from), $lte: new Date(to) },
+          [`${type}Id`]: { $exists: true }
+        }
+      },
+      {
+        $group: {
+          _id: {
+            day: { $dayOfMonth: '$dateCreated' },
+            month: { $month: '$dateCreated' },
+            year: { $year: '$dateCreated' }
+          },
+          dateCreated: { $first: '$dateCreated' },
+          total: { $sum: 1 }
+        }
+      },
+      {
+        $project: {
+          _id: 0
+        }
+      },
+      { $sort: { dateCreated: -1 } }
+    ])
+    .toArray()
+  MongoHelper.client.close()
+  res.json({ data })
+})
+
 const lookUp = (type = 'product') => [
   {
     $lookup: {
@@ -23,28 +62,27 @@ const lookUp = (type = 'product') => [
   {
     $group: {
       _id: `$${type}.name`,
-      price: { $first: `$${type}.price` },
       name: { $first: `$${type}.name` },
       purchased: {
         $sum: 1
       }
     }
   },
+
   {
     $project: {
       _id: 0,
-      price: 1,
       name: 1,
       purchased: 1
     }
-  }
+  },
+  { $sort: { purchased: -1 } }
 ]
 
-analytics.get('/:id', validateAdminToken, async (req: Request, res: Response) => {
+analytics.get('/aggregation/:type', validateAdminToken, async (req: Request, res: Response) => {
   const user = req.user as EmployeeToken
   const { from = subWeeks(Date.now(), 1).getTime(), to = addWeeks(1)(Date.now()).getTime() } = req.query
-  const { id } = req.params
-  const type = id === 'products' ? 'product' : 'offer'
+  const { type } = req.params
 
   await MongoHelper.connect()
   const data = await MongoHelper.db
